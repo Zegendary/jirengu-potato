28e1bfad5dbdde81613dcd374bc6f9206ac44826
Zegendary <zxw1992513@qq.com>
2019-02-28 14:48:25 +0800

    完成登出逻辑

--- src/components/Index/Index.tsx
+++ src/components/Index/Index.tsx
@@ -1,27 +1,52 @@
 import * as React from 'react';
 import { Button } from "antd";
+import axios from 'src/config/axios'
 
 interface IRouter {
 	history: any;
 }
 
-class Component extends React.Component<IRouter> {
+interface IIndexState {
+	user: any
+}
+
+class Index extends React.Component<IRouter,IIndexState> {
 
 	constructor(props: any){
 		super(props)
+		this.state = {
+			user: {}
+		}
 	}
 
-	login = ()=>{
+	async componentWillMount(){
+		await this.getMe()
+	}
+
+	getMe = async () => {
+		try{
+			const response = await axios.get('me')
+			this.setState({user: response.data})
+		}catch (e) {
+			// if(e.response.status === 401){
+			// 	this.props.history.push('/login')
+			// }
+		}
+	}
+
+	logout = ()=>{
+		localStorage.setItem('x-token','')
 		this.props.history.push('/login')
 	}
 
 	render() {
 		return (
 			<div className="Component">
-				<Button onClick={this.login}>登录</Button>
+				<p>欢迎，{this.state.user && this.state.user.account}</p>
+				<Button onClick={this.logout}>注销</Button>
 			</div>
 		);
 	}
 }
 
-export default Component;
+export default Index;
--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,4 +1,6 @@
 import axios from 'axios';
+// import { createBrowserHistory } from 'history';
+// const history = createBrowserHistory();
 
 const appID = "q5HRLsW98Fem9zhjBkQnurVR"
 const appSecret = "Yg2AF3XwqcnoQ1LPnV8gD27n"
@@ -32,6 +34,11 @@ instance.interceptors.response.use((response) => {
 	}
 	return response;
 },  (error) => {
+	if(error.response.status === 401){
+		console.log("重定向");
+		window.location.href = '/login'
+		// history.push('/login')
+	}
 	// Do something with response error
 	return Promise.reject(error);
 });
