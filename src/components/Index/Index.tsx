2ad65b99ebfcf8a97dc19b1fc8814e0078127b90
Zegendary <zxw1992513@qq.com>
2019-03-01 14:58:42 +0800

    优化代码

--- src/components/Index/Index.tsx
+++ src/components/Index/Index.tsx
@@ -24,14 +24,8 @@ class Index extends React.Component<IRouter,IIndexState> {
 	}
 
 	getMe = async () => {
-		try{
-			const response = await axios.get('me')
-			this.setState({user: response.data})
-		}catch (e) {
-			// if(e.response.status === 401){
-			// 	this.props.history.push('/login')
-			// }
-		}
+		const response = await axios.get('me');
+		this.setState({user: response.data})
 	}
 
 	logout = ()=>{
