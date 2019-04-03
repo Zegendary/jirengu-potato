4a2569ab957a99c980d0cc8c9f470e31d4a2692b
Zegendary <zxw1992513@qq.com>
2019-04-03 18:23:41 +0800

    部署

--- src/config/history.ts
+++ src/config/history.ts
@@ -1,4 +1,17 @@
 import { createBrowserHistory } from 'history';
-const history = createBrowserHistory();
+
+const ENV = process.env.NODE_ENV;
+let publicUrl:string = '';
+
+if(ENV === 'development'){
+	publicUrl = '/';
+}else if(ENV === 'production'){
+	publicUrl = '/jirengu-potato';
+}
+
+
+const history = createBrowserHistory({
+	basename: publicUrl
+});
 
 export default history
