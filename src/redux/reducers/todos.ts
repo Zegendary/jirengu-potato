3433824bef6906e2a49403a7d5e9d9542a285d33
Zegendary <zxw1992513@qq.com>
2019-04-22 13:55:34 +0800

    fix state

--- src/redux/reducers/todos.ts
+++ src/redux/reducers/todos.ts
@@ -3,7 +3,7 @@ import {ADD_TODO,INIT_TODOS,UPDATE_TODO,EDIT_TODO} from '../actionTypes'
 export default (state:any[] = [], action:any):any => {
 	switch (action.type){
 		case ADD_TODO:
-			return [...state,...action.payload];
+			return [...state,action.payload];
 		case INIT_TODOS:
 			return [...action.payload];
 		case UPDATE_TODO:
